bd61920fa8facda7f4316d8c48403fb0d901f66c
Pickachu Chen <pickachu1006@gmail.com>
2020-11-06 12:46:35 +0800

    20201106_add gif banner

--- js/all.js
+++ js/all.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
     //偵測sns line btn 手機版還是電腦版，連結不同
     var vw=$(window).width();
     if (vw <= 768) {
-        $(".line_link").attr("href", "https://bit.ly/2YszXqD");
+        $(".line_link").attr("href", "https://bit.ly/2Bpg9M6");
     } else {
         $(".line_link").attr("href", "https://line.me/R/ti/p/%40fhl1857j");
     }
